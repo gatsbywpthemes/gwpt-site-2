b6cd6762e339f24dde76b978e0a45f32b67ba020
PeHaa <info@pehaa.com>
2021-06-14 18:05:47 +0200

    script in layout

--- site/src/@gatsbywpthemes/gatsby-plugin-wp-seo/components/CustomHeadElements.js
+++ site/src/@gatsbywpthemes/gatsby-plugin-wp-seo/components/CustomHeadElements.js
@@ -2,15 +2,5 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 export const CustomHeadElements = (props) => {
-  return (
-    <Helmet>
-      <script
-        id="Cookiebot"
-        src="https://consent.cookiebot.com/uc.js"
-        data-cbid="b49b1a5f-acd1-49de-b4b0-6668ec151bf7"
-        data-blockingmode="auto"
-        type="text/javascript"
-      ></script>
-    </Helmet>
-  )
+  return <Helmet></Helmet>
 }
--- site/src/components/Layout.js
+++ site/src/components/Layout.js
@@ -6,6 +6,7 @@ import { Footer } from './footer'
 import { Global } from '@emotion/core'
 import { Grommet } from 'grommet'
 import { grommetStyles, globalStyles } from '../styles'
+import { Helmet } from 'react-helmet'
 
 import '../styles/scss/styles.scss'
 
@@ -20,40 +21,51 @@ export const Layout = ({ children, page, type = 'page' }) => {
     pageTemplate === 'WpFullWidthTemplate' ? 'fullWidth' : ''
 
   return (
-    <Grommet theme={grommetStyles}>
-      <Global styles={globalStyles(theme)} />
-      <Box
-        sx={{
-          '&.fullWidth': {
-            '.mainContainer': {
-              maxWidth: `100%`,
-              // px: 0,
-            },
-          },
-        }}
-        className={`${layoutClass}-${type} ${fullWidthClass}`}
-      >
-        <Header />
-
-        <main
+    <>
+      <Helmet>
+        <script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="b49b1a5f-acd1-49de-b4b0-6668ec151bf7"
+          data-blockingmode="auto"
+          type="text/javascript"
+        ></script>
+      </Helmet>
+      <Grommet theme={grommetStyles}>
+        <Global styles={globalStyles(theme)} />
+        <Box
           sx={{
-            py: `xxl`,
-            '.fullWidth &': {
-              py: 0,
-              // mt: -32,
+            '&.fullWidth': {
+              '.mainContainer': {
+                maxWidth: `100%`,
+                // px: 0,
+              },
             },
           }}
+          className={`${layoutClass}-${type} ${fullWidthClass}`}
         >
-          <Fragment>{children}</Fragment>
-        </main>
-        <Footer />
-        <script
-          id="CookieDeclaration"
-          src="https://consent.cookiebot.com/b49b1a5f-acd1-49de-b4b0-6668ec151bf7/cd.js"
-          type="text/javascript"
-          async
-        ></script>
-      </Box>
-    </Grommet>
+          <Header />
+
+          <main
+            sx={{
+              py: `xxl`,
+              '.fullWidth &': {
+                py: 0,
+                // mt: -32,
+              },
+            }}
+          >
+            <Fragment>{children}</Fragment>
+          </main>
+          <Footer />
+          <script
+            id="CookieDeclaration"
+            src="https://consent.cookiebot.com/b49b1a5f-acd1-49de-b4b0-6668ec151bf7/cd.js"
+            type="text/javascript"
+            async
+          ></script>
+        </Box>
+      </Grommet>
+    </>
   )
 }
