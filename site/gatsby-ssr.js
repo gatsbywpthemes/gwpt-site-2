e35f33163a99ddfedf48c7bb63e84a2f7cd4d404
PeHaa <info@pehaa.com>
2021-06-16 07:21:17 +0200

    switch to manual for ga and fb

--- site/gatsby-ssr.js
+++ site/gatsby-ssr.js
@@ -2,19 +2,25 @@ require('typeface-titillium-web')
 
 exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents()
-  headComponents.sort((x, y) => {
+  /*headComponents.sort((x, y) => {
     if (x.props && x.props['data-blockingmode'] === 'auto') {
       return -1
     } else if (y.props && y.props['data-blockingmode'] === 'auto') {
       return 1
     }
     return 0
-  })
+  })*/
   const newHeadComponents = headComponents.map((el) => {
     if (el.key?.startsWith('gatsby-plugin-google-gtag')) {
       el.props.type = 'text/plain'
       el.props['data-cookieconsent'] = 'statistics'
+    } else {
+      if (el.key?.startsWith('gatsby-plugin-facebook-pixel')) {
+        el.props.type = 'text/plain'
+        el.props['data-cookieconsent'] = 'marketing'
+      }
     }
+
     return el
   })
   replaceHeadComponents(newHeadComponents)
--- site/src/components/Layout.js
+++ site/src/components/Layout.js
@@ -28,8 +28,8 @@ export const Layout = ({ children, page, type = 'page', location }) => {
           id="Cookiebot"
           src="https://consent.cookiebot.com/uc.js"
           data-cbid="b49b1a5f-acd1-49de-b4b0-6668ec151bf7"
-          data-blockingmode="auto"
           type="text/javascript"
+          async
         ></script>
       </Helmet>
       <Grommet theme={grommetStyles}>
