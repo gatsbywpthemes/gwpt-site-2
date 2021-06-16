3bda6c8fa2d340daffa97b6d253174a601f1a3b9
PeHaa <info@pehaa.com>
2021-06-16 07:50:27 +0200

    try with wrapper

--- site/gatsby-browser.js
+++ site/gatsby-browser.js
@@ -1,6 +1,6 @@
 require('typeface-titillium-web')
 require('./src/styles/tailwind.css')
-/*
+
 const React = require('react')
 
 const Wrapper = (props) => {
@@ -8,7 +8,7 @@ const Wrapper = (props) => {
     console.log('wrapper useeffect')
     window.addEventListener(
       'CookiebotOnAccept',
-      function (e) {
+      function () {
         console.log(window.Cookiebot.consent)
         if (window.Cookiebot.consent.statistics) {
           //Execute code that sets statistics cookies
@@ -35,4 +35,3 @@ exports.wrapPageElement = ({ element, props }) => {
   // including location, data, etc - you don't need to pass it
   return <Wrapper {...props}>{element}</Wrapper>
 }
-*/
