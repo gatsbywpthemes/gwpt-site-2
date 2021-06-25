ccd447aa4e585bf61ea53a78ac6f8b17e7cdc6e2
Alexandra Spalato <alexaspalato@gmail.com>
2021-06-25 11:32:08 +0200

    subscribe sidebar

--- site/src/components/widgets/Newsletter.js
+++ site/src/components/widgets/Newsletter.js
@@ -3,13 +3,16 @@ import { jsx, Box, Flex, Input } from 'theme-ui'
 import { useState } from 'react'
 import addToMailchimp from 'gatsby-plugin-mailchimp'
 import { widgetStyles } from '../../styles'
+import subscribeBlockStyles from '../../styles/acfBlocksStyles/subscribeBlockStyles'
 
 export const Newsletter = (props) => {
+  const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
   const [msg, setMsg] = useState()
   const handleSubmit = (e) => {
     e.preventDefault()
-    addToMailchimp(email).then((data) => {
+    addToMailchimp(email, { FNAME: firstName }).then((data) => {
+      console.log('data', data)
       return data.result === 'success'
         ? setMsg(data.msg)
         : setMsg('This email has already subscribed, try with another one')
@@ -21,28 +24,43 @@ export const Newsletter = (props) => {
   return (
     <Box
       sx={{
-        ...widgetStyles.newsletter,
+        // ...widgetStyles.newsletter,
+        ...subscribeBlockStyles,
       }}
       {...props}
     >
       {msg ? (
         msg
       ) : (
-        <form onSubmit={handleSubmit}>
-          <h2 className="widget-title">Newsletter</h2>
-          <Flex>
+        <form onSubmit={handleSubmit} sx={{ width: '100% !important' }}>
+          <div>
+            <Input
+              placeholder="firstName"
+              name="firstname"
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+              sx={{
+                mr: 10,
+                color: 'nlInputColor',
+                bg: '#153651 !important',
+                width: '100% !important',
+                mb: 20,
+              }}
+            />
             <Input
               placeholder="Email address"
               name="email"
               type="email"
-              required
               value={email}
               onChange={handleChange}
-              sx={{ borderRadius: '5px 0 0 5px' }}
-              aria-label="Add your Email address to subsribe"
+              required
+              sx={{ mr: 10, color: 'nlInputColor', bg: '#153651 !important' }}
             />
+
             <button type="submit">Subscribe</button>
-          </Flex>
+          </div>
         </form>
       )}
     </Box>
